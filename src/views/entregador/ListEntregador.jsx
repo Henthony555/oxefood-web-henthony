863fac1a7d6fd960695a9ffa3c8ab8d2a7013f94
aluno <joao@gmail.com>
2023-05-10 21:32:02 -0300

    Fim Modal

--- src/views/entregador/ListEntregador.jsx
+++ src/views/entregador/ListEntregador.jsx
@@ -9,7 +9,7 @@ class ListEntregador extends React.Component {
 
         listaEntregadores: [],
         open: false,
-        selectedEntregador: null
+        dadosEntregador: null
 
     }
 
@@ -44,10 +44,23 @@ class ListEntregador extends React.Component {
         return dataFormatada
     };
 
+    formatarAtivo = (ativoParam) => {
+
+        let ativoFormatado
+
+        if(ativoParam == true){
+            ativoFormatado = "Sim";
+        }else{
+            ativoFormatado = "Não";
+        };
+
+        return ativoFormatado
+    };
+
     setOpen = (entregador) => {
         this.setState({
             open: !this.state.open,
-            selectedEntregador: entregador
+            dadosEntregador: entregador
         });
     }
 
@@ -149,16 +162,28 @@ class ListEntregador extends React.Component {
                     onClose={() => this.setOpen(null)} 
                     onOpen={() => this.setOpen(null)} 
                 >
-                    <Modal.Header>{this.state.selectedEntregador && this.state.selectedEntregador.nome}</Modal.Header>
+                    <Modal.Header>
+                      <Icon name='motorcycle' /> {this.state.dadosEntregador && this.state.dadosEntregador.nome}
+                    </Modal.Header>
                     <Modal.Content scrolling>
-                        {this.state.selectedEntregador && (
-                            <div>
-                                <p>CPF: {this.state.selectedEntregador.cpf}</p>
-                                <p>RG: {this.state.selectedEntregador.rg}</p>
-                                <p>Data de Nascimento: {this.formatarData(this.state.selectedEntregador.dataNascimento)}</p>
-                                <p>Fone Celular: {this.state.selectedEntregador.foneCelular}</p>
-                                <p>Fone Fixo: {this.state.selectedEntregador.foneFixo}</p>
-                            </div>
+                        {this.state.dadosEntregador && (
+                            <>
+                                <h5>CPF: {this.state.dadosEntregador.cpf}</h5>
+                                <h5>RG: {this.state.dadosEntregador.rg}</h5>
+                                <h5>Data de Nascimento: {this.formatarData(this.state.dadosEntregador.dataNascimento)}</h5>
+                                <h5>Fone Celular: {this.state.dadosEntregador.foneCelular}</h5>
+                                <h5>Fone Fixo: {this.state.dadosEntregador.foneFixo}</h5>
+                                <h5>qtdEntregasRealizadas: {this.state.dadosEntregador.qtdEntregasRealizadas}</h5>
+                                <h5>valorFrete: {this.state.dadosEntregador.valorFrete}</h5>
+                                <h5>enderecoRua: {this.state.dadosEntregador.enderecoRua}</h5>
+                                <h5>enderecoNumero: {this.state.dadosEntregador.enderecoNumero}</h5>
+                                <h5>enderecoBairro: {this.state.dadosEntregador.enderecoBairro}</h5>
+                                <h5>enderecoCidade: {this.state.dadosEntregador.enderecoCidade}</h5>
+                                <h5>enderecoCep: {this.state.dadosEntregador.enderecoCep}</h5>
+                                <h5>enderecoUf: {this.state.dadosEntregador.enderecoUf}</h5>
+                                <h5>enderecoComplemento: {this.state.dadosEntregador.enderecoComplemento}</h5>
+                                <h5>ativo: {this.formatarAtivo(this.state.dadosEntregador.ativo)}</h5>
+                            </>
                         )}
                     </Modal.Content>
                     <Modal.Actions>
